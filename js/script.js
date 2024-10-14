1303c37efe46c0b62bf9520305a8e98ec7176ce2
Joanna Koga-Naghmouchi <joanna_koga@tlen.pl>
2024-10-14 17:45:30 +0200

    Change function

--- js/script.js
+++ js/script.js
@@ -36,6 +36,12 @@ function playGame(playerInput){
   }
   displayResult(computerMove,playerMove);
 }
-document.getElementById('play-rock').addEventListener('click', playGame(1));
-document.getElementById('play-paper').addEventListener('click', playGame(2));
-document.getElementById('play-scissors').addEventListener('click', playGame(3));
+document.getElementById('play-rock').addEventListener('click', function() {
+  playGame(1)
+});
+document.getElementById('play-paper').addEventListener('click', function() {
+  playGame(2)
+});
+document.getElementById('play-scissors').addEventListener('click', function() {
+  playGame(3)
+});
